3aca38c5df9616005ab26f0a0ee934b815779f37
Voodoo3000 <m047wbm@mail.ru>
2019-07-24 18:00:19 +0600

    Update dockerBash.sh

--- dockerBash.sh
+++ dockerBash.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &&
 sudo yum -y install docker-ce docker-ce-cli containerd.io
-sudo -s
-base=https://github.com/docker/machine/releases/download/v0.16.0 &&
-curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
-sudo install /tmp/docker-machine /usr/local/bin/docker-machine
-exit
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo systemctl start docker
+#sudo -s
+#base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+#curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+#sudo install /tmp/docker-machine /usr/local/bin/docker-machine
+#exit
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#sudo systemctl start docker
